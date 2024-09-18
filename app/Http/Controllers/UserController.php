3e330ea67fde5f98dd3cad7f69fac4c2b30dbf2b
FaizNala <faiznala2@gmail.com>
2024-09-18 14:16:34 +0700

    Pertemuan 4 - Praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,15 +23,30 @@ class UserController extends Controller
         // ];
         // UserModel::where('username', 'customer-1')->update($data);
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'name' => 'Manager tiga',
-            'password' => Hash::make('12345'),
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'name' => 'Manager tiga',
+        //     'password' => Hash::make('12345'),
+        // ];
+        // UserModel::create($data);
 
-        $user = UserModel::all(); // ambil data dari m_user
+        // $user = UserModel::all(); // ambil data dari m_user
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::find(1);
+        // $user = UserModel::where('level_id', 1)->first();
+        // $user = UserModel::firstWhere('level_id', 1);
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::findOr(1, ['username', 'name'], function () {
+        //     abort(404);
+        // });
+
+        $user = UserModel::findOr(20, ['username', 'name'], function () {
+            abort(404);
+        });
+
         return view('user', ['data' => $user]);
     }
 }
