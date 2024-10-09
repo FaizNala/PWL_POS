f433002357b5542730e88e6d85a08e1664d7e950
FaizNala <faiznala2@gmail.com>
2024-10-09 15:17:28 +0700

    Pertemuan 7 - Praktikum 2

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,22 +5,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Foundation\Auth\User as Authenticatable; // implementas class Authenticatable
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class UserModel extends Authenticatable
 {
     use HasFactory;
-    protected $table = 'm_user'; // Mendefinisikan nama tabel
-    protected $primaryKey = 'user_id'; // Mendefinisikan primary key
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+    protected $fillable = ['level_id', 'username', 'nama', 'password', 'created_at', 'updated_at'];
 
     protected $hidden = ['password'];
 
     protected $casts = ['password' => 'hashed'];
 
-    // Relasi ke LevelModel (belongsTo)
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    public function getRoleName(): string
+    {
+        return $this->level->level_nama;
+    }
+
+    public function hasRole($role): bool
+    {
+        return $this->level->level_kode == $role;
     }
 }
