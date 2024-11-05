3e09ead7a907c86d886d486eeec6bf11c1ca794d
FaizNala <125951962+FaizNala@users.noreply.github.com>
2024-11-05 21:45:08 +0700

    Landing Page

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -8,6 +8,7 @@
     <title>Sistem POS</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="icon" href="{{ asset('tab_logo.png') }}" type="image/png">
 </head>
 
 <body>
